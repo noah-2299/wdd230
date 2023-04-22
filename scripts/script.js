cae16839ca152076aa7ed0bcbc9759f6c84530c2
noah-2299 <107367345+noah-2299@users.noreply.github.com>
2023-04-22 16:57:47 -0400

    First Draft of portal page

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    let date = new Date(document.lastModified);
+    let daysnames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = daysnames[date.getDay()];
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+  
+    let fullDateString = "Last Updated: " + day + ", " + month + "/" + date.getDate() + "/" + year + " at " + date.getHours() + ":" + date.getMinutes() + " EDT";
+    let element = document.getElementById("updated");
+    element.innerHTML = fullDateString;
+  };
+
