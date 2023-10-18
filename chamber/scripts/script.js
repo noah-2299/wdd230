d4f0c78065671125812d21b7d13a04f38e521130
noah-2299 <107367345+noah-2299@users.noreply.github.com>
2023-10-18 12:25:59 -0400

    WebP activity

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,27 @@
+
+  const apiKey = 'c5bf1eb7a0bb464a8b9140952231610';
+  const baseUrl = 'https://api.weatherapi.com/v1';
+  const currentWeather = '/current.json';
+
+  let Url = baseUrl + currentWeather +'?key=' +  apiKey + '&' +'q=22601';
+  async function getWeather(){
+    let response = await fetch(Url);
+    let data = await response.json();
+    return data;
+  }
+  getWeather().then(
+    data => {
+    const temp = document.getElementById('temp');
+    const weatherImg = document.getElementById('weather-img');
+    const humidity = document.getElementById('humidity');
+    const windChill = document.getElementById('wind-chill');
+    temp.textContent = data.current.temp_f;
+    weatherImg.src = data.current.condition.icon;
+    humidity.textContent = data.current.humidity;
+    windChill.textContent = data.current.wind_mph;
+
+    });
+
 window.onload = function() {
     let date = new Date(document.lastModified);
     let daysnames = [
@@ -21,8 +45,11 @@ window.onload = function() {
     let copySting = ` &copy ${year} || Noah C. Smith || Virginia, USA`;
     let element2 = document.getElementById('copy')
     element2.innerHTML = copySting;
-    
+
   };
+  
+  
+
 
 
   
