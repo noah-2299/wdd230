b3bcc9967d11523107145da263a6cfc08031d660
noah-2299 <107367345+noah-2299@users.noreply.github.com>
2023-10-23 14:55:53 -0400

    Native Lazy Loading of Images

---
+++ lesson03/scripts/last_modified.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    let date = new Date(document.lastModified);
+    let daysnames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = daysnames[date.getDay()];
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+  
+    let fullDateString = "Last Updated: " + day + ", " + month + "/" + date.getDate() + "/" + year + " at " + date.getHours() + ":" + date.getMinutes() + " EDT";
+    let element = document.getElementById("lastModified");
+    element.innerHTML = fullDateString;
+
+    let copySting = ` &copy ${year} || Noah C. Smith || Virginia, USA`;
+    let element2 = document.getElementById('copy')
+    element2.innerHTML = copySting;
+    
+  };
+
