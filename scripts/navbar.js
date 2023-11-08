ea443dd9cadec60773dcbaa79793a1ff316dd4fa
noah-2299 <107367345+noah-2299@users.noreply.github.com>
2023-11-08 09:02:10 -0500

    progress on join

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,1 +1,22 @@
 
+const navItems = document.getElementById('navbar');
+const hamburgerButton = document.getElementById('button');
+let isActive = false;
+
+function activateMenu(){
+    if (isActive){
+        navItems.classList.remove("active");
+        navItems.classList.add('inactive');
+        hamburgerButton.textContent = '≡';
+        isActive = false;
+    }    
+    else{
+        navItems.classList.remove('inactive');
+        navItems.classList.add('active');
+        hamburgerButton.textContent ='X';
+        isActive = true;
+    }
+    console.log('button pushed')
+}
+
+hamburgerButton.addEventListener('click', activateMenu);
