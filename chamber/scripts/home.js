80b0e5955f701af19fe5dfa7f25dfceaae76efc6
noah-2299 <107367345+noah-2299@users.noreply.github.com>
2023-11-27 20:53:26 -0500

    Final Changes

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -5,6 +5,17 @@ const apiKey = 'c5bf1eb7a0bb464a8b9140952231610';
   const weatherImg = document.getElementById('weather-img');
   const humidity = document.getElementById('humidity');
   const windChill = document.getElementById('wind-chill');
+  let d = new Date();
+  let today = d.getDay();
+  let daysnames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
 
   let Url = baseUrl + currentWeather +'?key=' +  apiKey + '&' +'q=22601';
   async function getWeather(){
@@ -30,8 +41,7 @@ const apiKey = 'c5bf1eb7a0bb464a8b9140952231610';
 
     let banner = document.getElementById('banner');
 
-    let d = new Date();
-    let today = d.getDay();
+    
     if (today == 1 || today == 2 || today == 3) {
       banner.classList.remove("active");
     }
@@ -40,3 +50,50 @@ const apiKey = 'c5bf1eb7a0bb464a8b9140952231610';
     }
     const close = document.getElementById('close');
     close.addEventListener('click',closeBanner); 
+
+    let forecast = baseUrl + '/forecast.json' +'?key=' +  apiKey + '&' +'q=22601' + '&days=3';
+    async function getForecast(){
+      let response = await fetch(forecast);
+      let object = await response.json();
+      let forecast_data = object.forecast
+      console.log(forecast_data);
+      displayForecast(forecast_data);
+    }
+
+
+getForecast();
+
+function displayForecast(forecast_data)
+{
+  let parent = document.getElementById('forecast');
+  let count = 0;
+  forecast_data.forecastday.forEach(day=> {
+    let card = document.createElement('div');
+    let day_name = document.createElement('p');
+    if (0 == count){
+      day_name.innerText = 'Today';
+      card.appendChild(day_name);
+
+    }
+    else if (1 == count){
+      day_name.innerText = 'Tomorrow';
+      card.appendChild(day_name);
+    }
+    else if (2== count){
+      day_name.innerText = `${daysnames[(today + count)]}`;
+    }
+    count ++;
+    let max_temp = document.createElement('p');
+    max_temp.innerText = `High: ${day.day.maxtemp_f} °F`;
+    let min_temp = document.createElement('p');
+    min_temp.innerText = `Low: ${day.day.mintemp_f} °F`
+    
+    card.appendChild(day_name);
+    card.appendChild(min_temp);
+    card.appendChild(max_temp);
+    parent.appendChild(card);
+    
+    console.log(temp);
+
+})
+};
